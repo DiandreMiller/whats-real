fe4de66e448d25a0e6a9e38e8ba220c74a52311f
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-08-12 23:08:34 -0400

    format: button component created before branch added

---
+++ app/(tabs)/AccessToCameraButton.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react"
+import { Text, View } from "@/components/Themed"
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+
+const AccessToCameraButton = () => {
+    
+}
